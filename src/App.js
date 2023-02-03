3764b9dd09cb0b480415e20bb03820bb01a14f93
Waleed Ahmed <>
2023-02-03 20:37:26 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <h1>
+            Hello world! Welcome to reactsetup without CRA.
+        </h1>
+    )
+}
+
+export default App
